78d2f8bb35fd9397d654bf96e559236b152327e3
suren reddy <suren@simplecue.com>
2012-11-17 16:34:38 +1000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hearly::Application.config.secret_token = 'c8bd373ae19e36ce815ec1032b0fc38d2bfbaf2921186f5bd469fe93dd5f8c321ec8b917ca25c2a7eb106a22dfe0535d689f26e569458e3018454671d5b3f3bd'
